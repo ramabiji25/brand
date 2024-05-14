866f2182bb7ee319eb2e667ad07d9f6773389558
ramabiji25 <96504836+Sahana20212@users.noreply.github.com>
2024-05-14 11:46:20 -0400

    SCRUM-9 I complted Brand services basics.

--- src/main/java/com/medline/brand/entity/Brand.java
+++ src/main/java/com/medline/brand/entity/Brand.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,7 @@ public class Brand {
 	@Column(name="brand_id")
 	private long brand_id;
 	@Column(name="brand_name")
+	@NotBlank(message = "Please Add Brand Name")
 	private String brand_name;
 
 }
--- src/main/java/com/medline/brand/service/BrandService.java
+++ src/main/java/com/medline/brand/service/BrandService.java
@@ -1,9 +1,12 @@
 package com.medline.brand.service;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.medline.brand.entity.Brand;
 import com.medline.brand.repository.BrandRepository;
@@ -29,4 +32,58 @@ public class BrandService {
 		return brandrepositoy.findById(brandByID).get();
 	}
 
+	public void deleteBrandtById(Long brandId) {
+		// TODO Auto-generated method stub
+		brandrepositoy.deleteById(brandId);
+		
+	}
+
+	//public Brand updatedBrand(Long brandId, Brand brand) {
+		// TODO Auto-generated method stub
+		 // Brand braDB = brandrepositoy.findById(brandId).get();
+
+	       // if(Objects.nonNull(brand.getBrand_name()) &&
+	       // !"".equalsIgnoreCase(brand.getBrand_name())) {
+	           // braDB.setBrand_name(brand.getBrand_name());
+	       // }
+
+	       // if(Objects.nonNull(brand.getBrandCode()) &&
+	               // !"".equalsIgnoreCase(brand.getBrandCode())) {
+	         //  braDB.setDepartmentCode(brand.getBrandCode());
+	       // }
+
+	       // if(Objects.nonNull(brand.getBrand_id()) &&
+	               // !"".equalsIgnoreCase(brand.getBrand_id())) {
+	          // braDB.setDepartmentAddress(brand.getBrandtAddress());
+	       // }
+
+	       // return brandrepositoy.save(braDB);
+	    //}
+
+	public Brand fetchBrandByName(String brandName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// // Update operation
+    //@PutMapping("/brand/{id}")
+ 
+   // public Brand
+    //updateBrand(@RequestBody Brand brand,
+                     //@PathVariable("id") Long brandId)
+   // {
+       // return brandService.updatebrand(
+            //brand, brandId);
+   //
+	
+
+
+
+public Brand fetchDepartmentByName(String brandName) {
+    return brandrepositoy.findByBrandNameIgnoreCase(brandName);
 }
+
+public Brand fetchBrandByNameIgnoreCase(String brandName) {
+	// TODO Auto-generated method stub
+	return null;
+}}
